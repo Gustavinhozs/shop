5309379df963a0e438b88285a737d5660fb521d2
Gustavinhozs <gustagollindao14@gmail.com>
2024-06-19 15:11:13 -0400

    xD
    botão ordenar lista
    az - za

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,14 +1,45 @@
+"use client"
 import Image from "next/image"
 import style from "../page.module.css"
+import { useEffect,useState } from "react";
 
-export default async function Main() {
+export default  function Main() {
+  const [listProduct, setProduct] = useState([]);
+  useEffect( ()=> {
+    const getProduct = async () => {
+      const response = await fetch("https://fakestoreapi.com/products/")
+      const data = await response.json();
+      setProduct(data);
+    }
+    getProduct()
+   }, []);
 
-  const response = await fetch("https://fakestoreapi.com/products/")
-  const data = await response.json();
+
+   const orderAz = () => {
+    const listAux = {...listProduct}.sort ((a,b) => 
+    a.title.localCompare(b.title) );
+    setProduct(listAux);
+   }
+
+
+   const orderZa = () =>{
+    let listAux = [...listProduct].sort((a,b) =>
+    a.title.localCompare(b.title) );
+
+    listAux = listAux.reverse();
+    setProduct(listAux0);
+     }
 
   return (
+    <>
+    <div className={style.filters}>
+      <div>
+         <button onClick={orderAz}> Az </button>
+         <button onClick={orderZa}> Za </button>
+      </div>
+    </div>
     <main className={style.body}>
-      {data.map((products) =>
+      {listProduct.map((products) =>
         <div className={style.card} key={products.id}>
           <h1>{products.title}</h1>
           <Image width={300} height={300} src={products.image} />
@@ -18,5 +49,6 @@ export default async function Main() {
         </div>
       )}
     </main>
+    </>
   );
 }
